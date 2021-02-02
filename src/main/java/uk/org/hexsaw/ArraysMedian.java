356f37f6b5a703114ad3c33344010b9490e346a0
Steve Westwood <steve.westwood@hexsaw.org.uk>
2021-02-02 15:15:37 +0000

    Handle empty arrays

--- src/main/java/uk/org/hexsaw/ArraysMedian.java
+++ src/main/java/uk/org/hexsaw/ArraysMedian.java
@@ -2,15 +2,34 @@ package uk.org.hexsaw;
 
 public class ArraysMedian {
 
-	public static int median(int[] intArray) {
+	public static Double median(int[] intArray) {
+        if (intArray.length == 0) {
+            return null;
+        }
         if (intArray.length %2 == 0) {
             // Get the median between two middle elements
             int lhsInt = intArray[(intArray.length/2) - 1];
             int rhsInt = intArray[intArray.length/2];
-            return (lhsInt + rhsInt) / 2;
+            return Double.valueOf((lhsInt + rhsInt)/2.0);
         } else {
             // odd-length array (only one middle element)
-            return intArray[intArray.length/2];
+            return Double.valueOf(intArray[intArray.length/2]);
+        }
+	}
+
+	public static Double medianOfTwoArrays(int[] firstIntArray, int[] secondIntArray) {
+        Double firstMedian = median(firstIntArray);
+        Double secondMedian = median(secondIntArray);
+        if (firstMedian == null && secondMedian == null) {
+            return null;
+        } else {
+            if (firstMedian == null)  {
+                return secondMedian;
+            } else if (secondMedian == null) {
+                return firstMedian;
+            } else {
+                return (firstMedian + secondMedian)/2;
+            }
         }
 	}
 
--- src/test/java/uk/org/hexsaw/ArraysMedianTest.java
+++ src/test/java/uk/org/hexsaw/ArraysMedianTest.java
@@ -1,6 +1,8 @@
 package uk.org.hexsaw;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 import org.junit.jupiter.api.Test;
 
 public class ArraysMedianTest {
@@ -16,5 +18,41 @@ public class ArraysMedianTest {
         int[] sampleIntArray = {1,3,5,7};
         assertEquals(4, ArraysMedian.median(sampleIntArray));
     }
+
+    @Test
+    void canHandleEmptyArrayWhenDeterminingMedianInOneArray() {
+        int[] emptyArray = {};
+        assertNull(ArraysMedian.median(emptyArray));
+    }
+
+    @Test
+    void validateMedianOfTwoArrays() {
+        int[] firstIntArray = {1,3,5};
+        int[] secondIntArray = {2,4,6};
+        assertEquals(3.5, ArraysMedian.medianOfTwoArrays(firstIntArray, secondIntArray));
+    }
+
+    @Test
+    void validateMedianOfTwoArraysWhenFirstOneEmpty() {
+        int[] firstIntArray = {};
+        int[] secondIntArray = {2,4,6};
+        assertEquals(4, ArraysMedian.medianOfTwoArrays(firstIntArray, secondIntArray));
+    }
+
+    @Test
+    void validateMedianOfTwoArraysWhenSecondOneEmpty() {
+        int[] firstIntArray = {1,3,5};
+        int[] secondIntArray = {};
+        assertEquals(3, ArraysMedian.medianOfTwoArrays(firstIntArray, secondIntArray));
+    }
+
+    @Test
+    void canHandleMedianForTwoEmptyArrays() {
+        int[] firstIntArray = {};
+        int[] secondIntArray = {};
+        assertNull(ArraysMedian.medianOfTwoArrays(firstIntArray, secondIntArray));
+    }
+
+
 
 }
